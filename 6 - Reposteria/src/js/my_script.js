abb0918f10d5872aa964a7f792ba12f1e4024542
Iago <iagooudi@gmail.com>
2026-01-27 10:40:40 +0100

    27-01-26

--- 6 - Reposteria/src/js/my_script.js
+++ 6 - Reposteria/src/js/my_script.js
@@ -4,7 +4,8 @@ const postres = [],
 
 const $d = document,
   $postres = $d.querySelector(".cards"),
-  $cestas = $d.querySelector(".carrito");
+  $cestas = $d.querySelector(".carrito"),
+  $dialog = $d.querySelector(".carrito__productos");
 
 async function ajax({ url, method, data, headers = {} }) {
   try {
@@ -44,65 +45,116 @@ async function ajax({ url, method, data, headers = {} }) {
   }
 }
 
-async function sumPostre(postre) {
-  const resp = await ajax({
-    url: `${url}/cestas/${postre.id}`,
-    method: "PATCH",
-    data: { cantidad: ++postre.cantidad }
-  });
-  if (resp.ok) {
-    cestas.push(resp.data);
-    renderPostres(postres, cestas);
-    renderCestas(cestas);
-  } else {
-    alert(`Error al añadir: ${resp.statusText}`);
-  }
-}
-
-async function resPostre(postre) {
-  const resp = await ajax({
-    url: `${url}/cestas/${postre.id}`,
-    method: "PATCH",
-    data:{ cantidad: --postre.cantidad }
-  });
-  if (resp.ok) {
-    cestas.push(resp.data);
-    renderPostres(postres, cestas);
-    renderCestas(cestas);
-  } else {
-    alert(`Error al añadir: ${resp.statusText}`);
-  }
-}
-
 async function addPostre(postre) {
   const resp = await ajax({
     url: `${url}/cestas`,
     method: "POST",
-    data: postre,
+    data: {
+      cantidad: 1,
+      postreId: postre.id,
+    },
   });
   if (resp.ok) {
     cestas.push(resp.data);
     renderPostres(postres, cestas);
-    renderCestas(cestas);
+    renderCesta(cestas);
   } else {
     alert(`Error al añadir: ${resp.statusText}`);
   }
 }
 
-$postres.addEventListener("click", async (ev) => {
-  ev.preventDefault();
+async function delPostre(id) {
+  const resp = await ajax({
+    url: `${url}/cestas/${id}`,
+    method: "DELETE",
+  });
 
-  let postre = postres.find((pos) => pos.id == ev.target.dataset.id);
-  if (postre) {
-    if ((ev.target.value == "+")) {
-      await sumPostre(postre);
-    } else if ((ev.target.value == "-")) {
-      await resPostre(postre);
+  if (resp.ok) {
+    let indice = cestas.findIndex((el) => el.id == id);
+    cestas.splice(indice, 1);
+    renderPostres(postres, cestas);
+    renderCesta(cestas)
+  } else {
+    alert(`Error al borrar: ${resp.statusText}`);
+  }
+}
+
+async function sumPostre(cesta) {
+  const resp = await ajax({
+    url: `${url}/cestas/${cesta.id}`,
+    method: "PATCH",
+    data: { cantidad: ++cesta.cantidad },
+  });
+  if (resp.ok) {
+    let indice = cestas.findIndex((el) => el.id == cesta.id);
+    cestas.splice(indice, 1, resp.data);
+    renderPostres(postres, cestas);
+    renderCesta(cestas);
+  } else {
+    alert(`Error al actualizar: ${resp.statusText}`);
+  }
+}
+
+async function resPostre(cesta) {
+  const resp = await ajax({
+    url: `${url}/cestas/${cesta.id}`,
+    method: "PATCH",
+    data: { cantidad: --cesta.cantidad },
+  });
+  if (resp.ok) {
+    if (cesta.cantidad <= 0) {
+      await delPostre(cesta.id);
     } else {
+      let indice = cestas.findIndex((el) => el.id == cesta.id);
+      cestas.splice(indice, 1, resp.data);
+      renderPostres(postres, cestas);
+      renderCesta(cestas);
+    }
+  } else {
+    alert(`Error al actualizar: ${resp.statusText}`);
+  }
+}
+
+async function delCesta() {
+  const resp = ajax({
+    url: `${url}/cestas`,
+    method: "DELETE",
+  });
+
+  if (resp.ok) {
+    cestas.splice(0, cestas.length);
+    renderPostres(postres, cestas);
+    renderCesta(cestas);
+  }
+}
+
+$postres.addEventListener("click", async (ev) => {
+  let postre = postres.find((el) => el.id === ev.target.dataset.id);
+  if (postre) {
+    let cesta = cestas.find((el) => el.postreId === ev.target.dataset.id);
+    if (ev.target.textContent == "+") {
+      await sumPostre(cesta);
+    } else if (ev.target.textContent == "-") {
+      await resPostre(cesta);
+    } else if (ev.target.classList.contains("card__add")) {
       await addPostre(postre);
     }
   }
 });
+
+async function borrarPostre(ev) {
+  ev.preventDefault();
+
+  let cesta = cestas.find((el) => (el.id = ev.target.dataset.id));
+  if (cesta) {
+    await delPostre(cesta.id);
+  } else if (ev.target.getAttribute("src") != null) {
+    await delCesta();
+  }
+}
+
+$cestas.addEventListener("click", borrarPostre);
+$dialog.addEventListener("click", borrarPostre);
 
 function renderPostres(postres, cestas) {
   $postres.innerHTML = postres.reduce((anterior, actual) => {
@@ -117,11 +169,11 @@ function renderPostres(postres, cestas) {
       ${
         postreCesta
           ? `<p class="card__add card__add--count">
-        <button class="card__btn" data_id="${actual.id}">-</button>
+        <button class="card__btn" data-id="${actual.id}">-</button>
         <span class="num">${postreCesta.cantidad}</span>
-        <button class="card__btn" data_id="${actual.id}">+</button>
+        <button class="card__btn" data-id="${actual.id}">+</button>
       </p>`
-          : `<p class="card__add" data_id="${actual.id}">Añadir al carro</p>`
+          : `<p class="card__add" data-id="${actual.id}">Añadir al carro</p>`
       }
 
       <section class="card__content">
@@ -135,15 +187,12 @@ function renderPostres(postres, cestas) {
   }, "");
 }
 
-function renderCestas(cestas) {
+function renderCesta(cestas) {
   let totalPostres = cestas.reduce(
     (anterior, actual) => anterior + actual.cantidad,
     0
   );
-  let totalPrecio = cestas.reduce(
-    (anterior, actual) => anterior + actual.price,
-    0
-  );
+  let totalPrecio = 0;
   $cestas.innerHTML = cestas.length
     ? `
         <h3>
@@ -156,14 +205,15 @@ function renderCestas(cestas) {
         <ul class="carrito__productos">
             ${cestas.reduce((anterior, actual) => {
               let postre = postres.find((el) => el.id == actual.postreId);
-              let totalPostre = parseFloat(actual.cantidad * postre.precio);
+              let totalPostre = actual.cantidad * postre.price;
+              totalPrecio += postre.price * actual.cantidad;
               return (
                 anterior +
                 `
                     <li class="producto">
                         <p class="producto__nombre">
                             ${postre.name}
-                            <button class="btn--delete" data_id="${actual.id}">X</button>
+                            <button class="btn--delete" data-id="${actual.id}">X</button>
                         </p>
                         <p class="producto__info">
                             <span class="cantidad">${actual.cantidad}x</span>
@@ -184,6 +234,33 @@ function renderCestas(cestas) {
                 <span class="carrito__total"></span>
             </h3>
             <p class="aviso">Sus articulos añadidos aparecerán aquí</p>`;
+
+  $dialog.innerHTML =
+    cestas.reduce((anterior, actual) => {
+      let postre = postres.find((el) => el.id == actual.postreId);
+      let totalPostre = actual.cantidad * postre.price;
+      return (
+        anterior +
+        `<li class="producto">
+        <figure>
+          <img src="${postre.image.thumbnail}" alt="" />
+        </figure>
+        <div role="region">
+          <p class="producto__nombre">
+            ${postre.name}
+            <button class="btn--delete" data-id="${actual.id}">X</button>
+          </p>
+          <p class="producto__info">
+            <span class="cantidad">${actual.cantidad}x</span>
+            <span class="precio">${postre.price}</span>
+            <span class="precio precio--total">${totalPostre}</span>
+          </p>
+        </div>
+      </li>`
+      );
+    }, "") +
+    `<p class="carrito__total">
+      Compra total<span class="precio">${totalPrecio}</span></p>`;
 }
 
 $d.addEventListener("DOMContentLoaded", (ev) => {
@@ -196,7 +273,7 @@ $d.addEventListener("DOMContentLoaded", (ev) => {
     .then((jsons) => {
       postres.splice(0, postres.length, ...jsons[0]);
       cestas.splice(0, cestas.length, ...jsons[1]);
-      renderCestas(cestas);
+      renderCesta(cestas);
       renderPostres(postres, cestas);
     })
     .catch((errors) => {
